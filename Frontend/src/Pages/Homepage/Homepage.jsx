60b17bf5005746b9ac50fd21191e62adf82c65fb
navid dadkhah <nnd1380@gmail.com>
2023-11-20 17:16:43 +0330

    adding navbar

--- Frontend/src/Pages/Homepage/Homepage.jsx
+++ Frontend/src/Pages/Homepage/Homepage.jsx
@@ -1,7 +1,22 @@
-import React from 'react'
-
+import { React, useEffect, useState } from 'react'
+import "../Homepage/Homepage.css"
 export const Homepage = () => {
+  useEffect(() => {
+
+  }, [])
   return (
-    <div>homepage</div>
+    <div className='home'>
+      <div className="navbar">
+        <ul className="navbar-ul">
+          <i><a href="#">Home</a></i>
+          <i><a href="#">About</a></i>
+          <i><a href="#">Admin</a></i>
+          <i><a href="#">Login</a></i>
+        </ul>
+      </div>
+      <div className="home-content">
+        homepage
+      </div>
+    </div>
   )
 }
