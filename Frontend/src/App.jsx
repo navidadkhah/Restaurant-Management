6f1153cad96285125cc3a27e6bda0364c03f6063
navid dadkhah <nnd1380@gmail.com>
2023-11-20 10:22:38 +0330

    adding homepage

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,7 +1,8 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import './App.css';
 import { Auth } from "./Pages/Auth/Auth";
-
+import { Homepage } from "./Pages/Homepage/Homepage"
+ 
 
 function App() {
   return (
@@ -10,12 +11,12 @@ function App() {
       <div className="blur" style={{ top: "36%", left: "-8rem" }}></div>
       <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Auth />}>
-        </Route>
+        <Route path="/" element={<Auth />}></Route>
+        <Route path="/Home" element={<Homepage />}></Route>
       </Routes>
     </BrowserRouter>
     </div>
   );
-}
+} 
 
 export default App;
