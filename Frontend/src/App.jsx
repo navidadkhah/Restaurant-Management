510cd1d940e516e5ece8367f7fe46e32d1de6839
Mohammad Vatankhah <mamadvatan80@gmail.com>
2023-12-12 17:55:21 +0330

    restaurant and admin login pages added

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,8 +1,9 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import './App.css';
+import "./App.css";
 import { Auth } from "./Pages/Auth/Auth";
 import { Homepage } from "./Pages/Homepages/Homepage";
-
+import { AdminLogin } from "./Pages/AdminPanel/AdminLogin";
+import { RestaurantAdminLogin } from "./Pages/RestaurantAdminPanel/RestaurantAdminLogin";
 
 function App() {
   return (
@@ -10,16 +11,15 @@ function App() {
       <div className="blur" style={{ top: "-18%", right: "0" }}></div>
       <div className="blur" style={{ top: "36%", left: "-8rem" }}></div>
       <BrowserRouter>
-      <Routes>
+        <Routes>
           <Route path="/" element={<Auth />}></Route>
           <Route path="/home" element={<Homepage />}></Route>
-
-      </Routes>
-    </BrowserRouter>
+          <Route path="/web-admin" element={<AdminLogin />}></Route>
+          <Route path="/restaurant-admin" element={<RestaurantAdminLogin />}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
-
-} 
-
+}
 
 export default App;
