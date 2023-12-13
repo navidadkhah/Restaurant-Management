f12c8f55b4280056a55438fdac9557ffbd8f8fc3
navid dadkhah <nnd1380@gmail.com>
2023-12-13 14:53:28 +0330

    adding table

--- Frontend/src/Pages/AdminPanel/AdminDashboard.jsx
+++ Frontend/src/Pages/AdminPanel/AdminDashboard.jsx
@@ -1,18 +1,15 @@
 import React from "react";
 import "./AdminDashboard.css";
 import { Table } from "../Components/Table/Table";
+import { CardPanel } from "../Components/CardPanel/CardPanel";
 
 export const AdminDashboard = () => {
   return (
     <div className="admin-dash">
-      <div className="admin-block">
-        <span>Current Restaurants</span>
-        <span>20</span>
-      </div>
+      <CardPanel title="Current Restaurants" number="20" />
       <div className="admin-lists">
-        <div className="admin-list-buttons">
+        <div className="admin-list-button">
           <button>add</button>
-          {/* <button></button> */}
         </div>
         <Table></Table>
       </div>
--- Frontend/src/Pages/Components/Table/Table.jsx
+++ Frontend/src/Pages/Components/Table/Table.jsx
@@ -1,7 +1,37 @@
-import React from 'react'
+import React from "react";
+import './Table.css'
 
 export const Table = () => {
   return (
-    <div><tr><td>sdsd</td></tr></div>
-  )
-}
+    <div>
+      <table id="customers">
+        <tr>
+          <th>Name</th>
+          <th>Type</th>
+          <th>Delete</th>
+        </tr>
+        <tr>
+          <td>Alfreds Futterkiste</td>
+          <td>Maria Anders</td>
+          <td>Germany</td>
+        </tr>
+        <tr>
+          <td>Berglunds snabbköp</td>
+          <td>Christina Berglund</td>
+          <td>Sweden</td>
+        </tr>
+        <tr>
+          <td>Centro comercial Moctezuma</td>
+          <td>Francisco Chang</td>
+          <td>Mexico</td>
+        </tr>
+        <tr>
+          <td>Ernst Handel</td>
+          <td>Roland Mendel</td>
+          <td>Austria</td>
+        </tr>
+       
+      </table>
+    </div>
+  );
+};
