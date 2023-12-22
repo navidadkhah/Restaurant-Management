bc211cdf703b386cfbe5dd23f46832a80b359e07
negarhonarvar <negar_hrs@yahoo.com>
2023-12-22 17:13:10 +0330

    Models Created

--- Backend/dashboard/admin.py
+++ Backend/dashboard/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import RestaurantAdminMenuModel
+from .models import RestaurantAdminMenuModel,RestaurantAdminProfileModel
 
 
 # Register your models here.
 
 admin.site.register(RestaurantAdminMenuModel)
+admin.site.register(RestaurantAdminProfileModel)
--- Backend/dashboard/models.py
+++ Backend/dashboard/models.py
@@ -6,18 +6,18 @@ class RestaurantAdminMenuModel(models.Model):
     id = models.AutoField(primary_key=True)
     foodName = models.CharField(max_length=50)
     foodPrice = models.IntegerField()
-    foodDescription = models.CharField(max_length=100)
+    foodDescription = models.CharField(max_length=200)
     foodType = models.EmailField(max_length=50)
-    foodImage = models.ImageField( blank=False,upload_to='images/')
+    foodImage = models.ImageField( blank=False,upload_to='foodImages/')
 
     def __str__(self):
         return self.foodName
-    # in this section we can update the uncritical restaurant info
+    
+# in this section we can update the uncritical restaurant info
 class RestaurantAdminProfileModel(models.Model):
     id = models.AutoField(primary_key=True)
-    restaurantDescription = models.CharField(max_length=100)
-    restaurantImage = models.ImageField( blank=False,upload_to='images/')
+    restaurantDescription = models.CharField(max_length=200)
+    restaurantImage = models.ImageField( blank=False,upload_to='restaurantImages/')
 
-    def __str__(self):
-        return self.firstName+ " " + self.lastName
+
     
