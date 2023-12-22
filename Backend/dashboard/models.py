e98c8a88f1659cd83a097ad080065ee855884a13
negarhonarvar <negar_hrs@yahoo.com>
2023-12-22 18:42:08 +0330

    site model added

--- Backend/dashboard/models.py
+++ Backend/dashboard/models.py
@@ -19,5 +19,24 @@ class RestaurantAdminProfileModel(models.Model):
     restaurantDescription = models.CharField(max_length=200)
     restaurantImage = models.ImageField( blank=False,upload_to='restaurantImages/')
 
+# deffing site admin
+class siteAdminModel(models.Model):
+    id = models.AutoField(primary_key=True)
+    restaurantName = models.CharField(max_length=50)
+    restaurantDescription = models.CharField(max_length=200)
+    restaurantType = models.EmailField(max_length=50)
+    restaurantImage = models.ImageField( blank=False,upload_to='restaurantImages/')
+
+    def __str__(self):
+        return self.restaurantName
+
+# deffing restaurant admin by site admin
+class RestaurantAdminModel(models.Model):
+    id =  models.AutoField(primary_key=True)
+    restaurantUsername = models.CharField(max_length = 50)
+    restaurantPassword = models.CharField(max_length = 50)
+
+    def __str__(self) :
+        return self.restaurantUsername
 
     
