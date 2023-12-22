7bb36881345139e132d100feb8a344016aa5a386
arman <army.ghafarnia@gmail.com>
2023-12-22 20:54:17 +0330

    save user in local storage added

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,23 +2,30 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import { Auth } from "./Pages/Auth/Auth";
 import { Homepage } from "./Pages/Homepages/Homepage";
-import {AdminDashboard} from "./Pages/AdminPanel/AdminDashboard"
+import { AdminDashboard } from "./Pages/AdminPanel/AdminDashboard";
 import { AdminLogin } from "./Pages/AdminPanel/AdminLogin";
 import { RestaurantAdminLogin } from "./Pages/RestaurantAdminPanel/RestaurantAdminLogin";
-
+import { useEffect, useState } from "react";
 
 function App() {
+  const [user, setUser] = useState();
+  useEffect(() => {
+    setUser(JSON.parse(localStorage.getItem("User")));
+  }, []);
   return (
     <div className="App">
       <div className="blur" style={{ top: "-18%", right: "0" }}></div>
       <div className="blur" style={{ top: "36%", left: "-8rem" }}></div>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Auth />}></Route>
+          <Route path="/" element={user ? <Homepage /> : <Auth />}></Route>
           <Route path="/home" element={<Homepage />}></Route>
           <Route path="/admin-dashboard" element={<AdminDashboard />}></Route>
           <Route path="/web-admin" element={<AdminLogin />}></Route>
-          <Route path="/restaurant-admin" element={<RestaurantAdminLogin />}></Route>
+          <Route
+            path="/restaurant-admin"
+            element={<RestaurantAdminLogin />}
+          ></Route>
         </Routes>
       </BrowserRouter>
     </div>
--- Frontend/src/Pages/Auth/Auth.jsx
+++ Frontend/src/Pages/Auth/Auth.jsx
@@ -2,9 +2,10 @@ import React, { useRef } from "react";
 import { useState } from "react";
 import { IoLogoSlack } from "react-icons/io";
 import { login_API, signup_API } from "../../api/AuthController";
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.min.css';
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.min.css";
 import "./Auth.css";
+import { json } from "react-router-dom";
 export const Auth = () => {
   const [isSignup, setIsSignup] = useState(true);
   const confirmRef = useRef();
@@ -17,7 +18,6 @@ export const Auth = () => {
     password: "",
   });
 
-
   const handleChange = (e) => {
     setData({ ...data, [e.target.name]: e.target.value });
   };
@@ -26,10 +26,10 @@ export const Auth = () => {
     e.preventDefault();
     if (isSignup) {
       let flag_is_fill = true;
-      let flag_is_valid_phone = true
-      let flag_is_pass_valid = true
-      let flag_is_email_valid = true
-     
+      let flag_is_valid_phone = true;
+      let flag_is_pass_valid = true;
+      let flag_is_email_valid = true;
+
       for (let key in data) {
         if (data[key] === "") {
           flag_is_fill = false;
@@ -39,57 +39,61 @@ export const Auth = () => {
 
       if (flag_is_fill) {
         let phone_regex = /^\d{11}$/;
-        let regex_email = /^[a-zA-Z0-9]+@(?:[a-zA-Z0-9]+\.)+[A-Za-z]+$/
+        let regex_email = /^[a-zA-Z0-9]+@(?:[a-zA-Z0-9]+\.)+[A-Za-z]+$/;
         if (!phone_regex.test(data.phoneNumber)) {
           flag_is_valid_phone = false;
-          notify('Invalid phone number!', "error")
+          notify("Invalid phone number!", "error");
         } else {
           flag_is_valid_phone = true;
         }
 
-
         if (!regex_email.test(data.email)) {
           flag_is_email_valid = false;
-          notify('Invalid Email Format!', "error")
+          notify("Invalid Email Format!", "error");
         } else {
           flag_is_email_valid = true;
         }
-   
+
         if (data.password !== confirmRef.current.value) {
           flag_is_pass_valid = false;
-          notify('Passwords does not match!', "error")
+          notify("Passwords does not match!", "error");
         } else {
           flag_is_pass_valid = true;
         }
-      } 
-      else{
-        notify('Please fill all fields!', "error")
+      } else {
+        notify("Please fill all fields!", "error");
       }
-  
-      if (flag_is_fill & flag_is_valid_phone & flag_is_pass_valid & flag_is_email_valid) {
+
+      if (
+        flag_is_fill &
+        flag_is_valid_phone &
+        flag_is_pass_valid &
+        flag_is_email_valid
+      ) {
         delete data.confirmPassword;
         console.log(data);
         try {
           const res = await signup_API(data);
-          notify(res.data, "success")
+          notify(res.data, "success");
         } catch (error) {
-          notify(error.response.data, "error")
+          notify(error.response.data, "error");
         }
       }
     } else {
       const loginData = { email: data.email, password: data.password };
       try {
         const res = await login_API(loginData);
-        localStorage.setItem("Token", JSON.stringify(res.data.token))
-        notify("successfylly logged in!", "success")
+        localStorage.setItem("Token", JSON.stringify(res.data.token));
+        localStorage.setItem("User", JSON.stringify(res.data.detail));
+        notify("successfylly logged in!", "success");
       } catch (error) {
-        notify(error.response.data, "error")
+        notify(error.response.data, "error");
       }
     }
   };
 
   const notify = (msg, type) => {
-    if (type==="error"){
+    if (type === "error") {
       toast.error(msg, {
         position: "top-left",
         autoClose: 3000,
@@ -99,9 +103,8 @@ export const Auth = () => {
         draggable: false,
         progress: undefined,
         theme: "light",
-    });
-    }
-    else if (type === "success"){
+      });
+    } else if (type === "success") {
       toast.success(msg, {
         position: "top-left",
         autoClose: 3000,
@@ -113,7 +116,7 @@ export const Auth = () => {
         theme: "light",
       });
     }
-  }
+  };
 
   const resetForm = () => {
     setData({
@@ -216,7 +219,7 @@ export const Auth = () => {
               />
             )}
           </div>
-         
+
           <span
             onClick={() => {
               setIsSignup((prev) => !prev);
@@ -227,7 +230,6 @@ export const Auth = () => {
             {isSignup
               ? "Already have an account? Login."
               : "Don't have an account. Sign up."}
-          
           </span>
           <button
             className="button"
@@ -239,8 +241,7 @@ export const Auth = () => {
           </button>
         </form>
       </div>
-       <ToastContainer />
+      <ToastContainer />
     </div>
-    
   );
 };
