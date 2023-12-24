e3874261fcea0917448fd7fc9bcd4e17a3676be2
navid dadkhah <nnd1380@gmail.com>
2023-12-24 16:07:55 +0330

    fix backend apis

--- Backend/dashboard/urls.py
+++ Backend/dashboard/urls.py
@@ -6,6 +6,11 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('createFood/', view=views.CreateFoodView),
     path('allMenu/', view=views.allMenuView),
+    path('updateRestaurant/<int:pk>/', view=views.updateResInfoView),
+    path('CreateRestaurantAdmin/', view=views.CreateRestaurantAdmin),
+    path('CreateSiteAdmin/',view=views.CreateSiteAdmin),
+    path('GetCreateSiteAdmin/',view=views.GetCreateSiteAdmin),
+    path('GetRestaurantAdmin/',view=views.GetRestaurantAdmin)
 ]
 
 if settings.DEBUG:
