86a99b1fc7e948e07d3ec4bed8f4c4ce941a38f7
negarhonarvar <negar_hrs@yahoo.com>
2023-12-24 18:26:44 +0330

    modifications

--- Backend/dashboard/admin.py
+++ Backend/dashboard/admin.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
-from .models import RestaurantAdminMenuModel,RestaurantAdminProfileModel,siteAdminModel,RestaurantAdminModel
+from .models import RestaurantAdminMenuModel,RestaurantUpdateInfoModel,siteAdminModel,RestaurantAdminModel
 
 
 # Register your models here.
 
 admin.site.register(RestaurantAdminMenuModel)
-admin.site.register(RestaurantAdminProfileModel)
+admin.site.register(RestaurantUpdateInfoModel)
 admin.site.register(siteAdminModel)
 admin.site.register(RestaurantAdminModel)
