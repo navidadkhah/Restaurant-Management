ca833bc00366aaf0c3f91f4ba99b75ebdbb19f9b
negarhonarvar <negar_hrs@yahoo.com>
2023-12-25 16:42:06 +0330

    views corrected but still include patch

--- Backend/dashboard/views.py
+++ Backend/dashboard/views.py
@@ -1,20 +1,20 @@
 from django.shortcuts import render
-from dashboard.models import RestaurantAdminMenuModel,RestaurantAdminProfileModel,RestaurantAdminModel , siteAdminModel
+from dashboard.models import RestaurantMenuModel, siteAdminModel
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from .serializers import RestaurantAdminMenuModelSerializer,RestaurantAdminProfileModelSerializer,RestaurantAdminLoginSerializer,siteAdminModelSerializer
+from .serializers import RestaurantMenuModelSerializer,RestaurantAdminProfileModelSerializer,RestaurantAdminLoginSerializer,siteAdminModelSerializer, RestaurantAdminGetMenuSerializer
 from drf_yasg.utils import swagger_auto_schema
 
 # create food by restaurant admin
-@swagger_auto_schema(method='POST', request_body=RestaurantAdminMenuModelSerializer)
+@swagger_auto_schema(method='POST', request_body=RestaurantMenuModelSerializer)
 @api_view(["POST"])
 def CreateFoodView(request):
-    serializer = RestaurantAdminMenuModelSerializer(data=request.data)
+    serializer = RestaurantMenuModelSerializer(data=request.data)
     if serializer.is_valid():
         try:
-           RestaurantAdminMenuModel.objects.get(foodName=request.data["foodName"])
-        except RestaurantAdminMenuModel.DoesNotExist:
+           RestaurantMenuModel.objects.get(foodName=request.data["foodName"])
+        except RestaurantMenuModel.DoesNotExist:
            serializer.save()
            return Response("Food successfully created", status=status.HTTP_201_CREATED)
         return Response("This food name is already exists!", status=status.HTTP_401_UNAUTHORIZED)
@@ -24,72 +24,57 @@ def CreateFoodView(request):
 @swagger_auto_schema(method='GET')
 @api_view(["GET"])
 def allMenuView(request):
-    users = RestaurantAdminMenuModel.objects.all()
-    serializer = RestaurantAdminMenuModelSerializer(users, many=True)
+    users = RestaurantMenuModel.objects.all()
+    serializer = RestaurantMenuModelSerializer(users, many=True)
     return Response(serializer.data)
 
 
-# updating restaurant uncritical info by res-admin
-@swagger_auto_schema(method='PATCH')
-@api_view(["PATCH"])
-def updateResInfoView(request , pk):
-    try:
-        user = RestaurantAdminProfileModel.objects.get(pk=pk)
-    except RestaurantAdminProfileModel.DoesNotExist:
-        return Response({"error": "User not found"}, status=404)
+# # updating restaurant uncritical info by res-admin
+# @swagger_auto_schema(method='PATCH')
+# @api_view(["PATCH"])
+# def updateResInfoView(request , pk):
+#     try:
+#         user = RestaurantAdminProfileModel.objects.get(pk=pk)
+#     except RestaurantAdminProfileModel.DoesNotExist:
+#         return Response({"error": "User not found"}, status=404)
 
-    data_to_update = {}
-    if 'restaurantDescription' in request.data:
-        data_to_update['restaurantDescription'] = request.data['restaurantDescription']
-    if 'restaurantImage' in request.data:
-        data_to_update['restaurantImage'] = request.data['restaurantImage']
+#     data_to_update = {}
+#     if 'restaurantDescription' in request.data:
+#         data_to_update['restaurantDescription'] = request.data['restaurantDescription']
+#     if 'restaurantImage' in request.data:
+#         data_to_update['restaurantImage'] = request.data['restaurantImage']
 
-    serializer = RestaurantAdminProfileModelSerializer(user, data=data_to_update, partial=True)
+#     serializer = RestaurantAdminProfileModelSerializer(user, data=data_to_update, partial=True)
     
-    if serializer.is_valid():
-        serializer.save()
-        return Response(serializer.data)
+#     if serializer.is_valid():
+#         serializer.save()
+#         return Response(serializer.data)
     
-    return Response(serializer.errors, status=400)
+#     return Response(serializer.errors, status=400)
 
 # create resturant by site admin
 @swagger_auto_schema(method='POST', request_body=siteAdminModelSerializer)
 @api_view(["POST"])
-def CreateSiteAdmin(request):
+def CreateRestaurant(request):
     serializer = siteAdminModelSerializer(data=request.data)
     if serializer.is_valid():
         try:
            siteAdminModel.objects.get(restaurantName=request.data["restaurantName"])
         except siteAdminModel.DoesNotExist:
-            serializer.save()  
-            return Response("Restaurant profile successfully created", status=status.HTTP_201_CREATED)
+            try:
+               siteAdminModel.objects.get(restaurantUsername=request.data["restaurantUsername"])
+            except siteAdminModel.DoesNotExist:
+               serializer.save()  
+               return Response("Restaurant profile successfully created", status=status.HTTP_201_CREATED)
+            return Response("This username already exist!", status=status.HTTP_401_UNAUTHORIZED)
         return Response("This Restaurant already exist!", status=status.HTTP_401_UNAUTHORIZED)
     return Response("Some fields are missing", status=status.HTTP_400_BAD_REQUEST)
 
-
+# returns all the restaurants in homepage
 @swagger_auto_schema(method='GET')
 @api_view(["GET"])
-def GetCreateSiteAdmin(request):
+def GetAllRestaurants(request):
     users = siteAdminModel.objects.all()
-    serializer = siteAdminModelSerializer(users, many=True)
+    serializer = RestaurantAdminGetMenuSerializer(users, many=True)
     return Response(serializer.data)
 
-# create username and password for restaurant admin
-@swagger_auto_schema(method='POST', request_body=RestaurantAdminLoginSerializer)
-@api_view(["POST"])
-def CreateRestaurantAdmin(request):
-    serializer = RestaurantAdminLoginSerializer(data=request.data)
-    if serializer.is_valid():
-        try:
-           RestaurantAdminLoginSerializer.objects.get(restaurantUsername=request.data["restaurantUsername"])
-        except RestaurantAdminModel.DoesNotExist:
-             return Response("Profile successfully created", status=status.HTTP_201_CREATED)
-        return Response("This Username already exist!", status=status.HTTP_401_UNAUTHORIZED)
-    return Response("Some fields are missing", status=status.HTTP_400_BAD_REQUEST)
-
-@swagger_auto_schema(method='GET')
-@api_view(["GET"])
-def GetRestaurantAdmin(request):
-    users = RestaurantAdminModel.objects.all()
-    serializer = RestaurantAdminLoginSerializer(users, many=True)
-    return Response(serializer.data)
