2f7ec7b601eadd1ef861c4bceccdd677babccfe0
negarhonarvar <negar_hrs@yahoo.com>
2023-12-26 17:33:49 +0330

    changing urls

--- Backend/authenticate/urls.py
+++ Backend/authenticate/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('signup/', view=views.signupView),
-    path('login/', view=views.loginView),
-    path('users/', view=views.allusers),
+    path('signup/', view=views.signupView , name='signupView'),
+    path('login/', view=views.loginView, name='loginView'),
+    path('users/', view=views.allusers, name='allusers'),
 ]
