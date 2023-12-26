9d7df0a1948b2e3774596ec2726001072b1d5d5e
arman <army.ghafarnia@gmail.com>
2023-12-26 15:30:55 +0330

    admin panel restaurants table fixed

--- Frontend/src/Pages/Components/Table/Table.jsx
+++ Frontend/src/Pages/Components/Table/Table.jsx
@@ -1,84 +1,22 @@
-import React from "react";
-import './Table.css'
+import "./Table.css";
 
 export const Table = (props) => {
   return (
-    <table id="customers">
-      <tr>
-        <th>Name</th>
-        <th>Type</th>
-        <th>Delete</th>
-      </tr>
-      <tr>
-        <td>Alfreds Futterkiste</td>
-        <td>Maria Anders</td>
-        <td>Germany</td>
-      </tr>
-      <tr>
-        <td>Berglunds snabbköp</td>
-        <td>Christina Berglund</td>
-        <td>Sweden</td>
-      </tr>
-      <tr>
-        <td>Centro comercial Moctezuma</td>
-        <td>Francisco Chang</td>
-        <td>Mexico</td>
-      </tr>
-      <tr>
-        <td>Ernst Handel</td>
-        <td>Roland Mendel</td>
-        <td>Austria</td>
-      </tr>
-      <tr>
-        <td>Ernst Handel</td>
-        <td>Roland Mendel</td>
-        <td>Austria</td>
-      </tr>
-      <tr>
-        <td>Ernst Handel</td>
-        <td>Roland Mendel</td>
-        <td>Austria</td>
-      </tr>{" "}
-      <tr>
-        <td>Ernst Handel</td>
-        <td>Roland Mendel</td>
-        <td>Austria</td>
-      </tr>{" "}
-      <tr>
-        <td>Ernst Handel</td>
-        <td>Roland Mendel</td>
-        <td>Austria</td>
-      </tr>{" "}
-      <tr>
-        <td>Ernst Handel</td>
-        <td>Roland Mendel</td>
-        <td>Austria</td>
-      </tr>
-      <tr>
-        <td>Ernst Handel</td>
-        <td>Roland Mendel</td>
-        <td>Austria</td>
-      </tr>
-      <tr>
-        <td>Ernst Handel</td>
-        <td>Roland Mendel</td>
-        <td>Austria</td>
-      </tr>
-      <tr>
-        <td>Ernst Handel</td>
-        <td>Roland Mendel</td>
-        <td>Austria</td>
-      </tr>
-      <tr>
-        <td>Ernst Handel</td>
-        <td>Roland Mendel</td>
-        <td>Austria</td>
-      </tr>
-      <tr>
-        <td>Ernst Handel</td>
-        <td>Roland Mendel</td>
-        <td>Austria</td>
-      </tr>
-    </table>
+    props.restaurants && (
+      <table id="customers">
+        <tr>
+          <th>Name</th>
+          <th>Type</th>
+          <th>Address</th>
+        </tr>
+        {props.restaurants.map((res) => (
+          <tr>
+            <td>{res.restaurantName}</td>
+            <td>{res.restaurantType}</td>
+            <td>{res.restaurantLocation}</td>
+          </tr>
+        ))}
+      </table>
+    )
   );
 };
