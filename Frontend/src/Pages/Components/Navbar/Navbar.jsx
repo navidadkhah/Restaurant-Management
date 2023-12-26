e5709f382753e9d7e62d6802faf9f72db06db50a
navid dadkhah <nnd1380@gmail.com>
2023-12-26 19:46:43 +0330

    try to fix search

--- Frontend/src/Pages/Components/Navbar/Navbar.jsx
+++ Frontend/src/Pages/Components/Navbar/Navbar.jsx
@@ -5,33 +5,51 @@ import { UserProfile } from "../../UserProfile/UserProfile";
 
 import "./Navbar.css";
 
-export const Navbar = ({ setRestaurants, restaurants }) => {
-  const [isChecked, setIsChecked] = useState(true);
-  const [search, setSearch] = useState("");
+export const Navbar = ({ restaurants, isSearch, setSearchRestaurants }) => {
+
+  const [isChecked, setIsChecked] = useState(true);;
   const [user, setUser] = useState();
 
   useEffect(() => {
     setUser(JSON.parse(localStorage.getItem("User")));
   }, []);
 
-
   const handleCheckboxChange = () => {
     setIsChecked(!isChecked);
   };
 
-  const profileHandle = () => {
-    <Navigate to={UserProfile} />;
+  const handleSearch = (e) => {
+    const searchValue = e.target.value;
+    if(searchValue ===''){
+      isSearch(0)
+    }else{
+      isSearch(1)
+    }
+    const filteredItems = restaurants.filter((rs) => {
+      if (
+        rs.restaurantType.toLowerCase().includes(e.target.value.toLowerCase())
+      ) {
+        setSearchRestaurants(rs);
+        console.log(rs)
+      }
+    });
+
+    //keep filtered items in another state
+
+    //     restaurants.filter(res =>{
+    //       if(res.restaurantType.toLowerCase().includes(e.target.value.toLowerCase()))
+    //       setRestaurants(res)
+    //     }
+    //       // console.log(res.restaurantType.toLowerCase().includes(e.target.value.toLowerCase()))
+    //       // setRestaurants(res.restaurantType.toLowerCase().includes(e.target.value.toLowerCase()))
+    // )
+    // restaurants.filter((res) => {
+    //   if (res.restaurantName.startsWith(e.target.value)) {
+    //     setRestaurants(res);
+    //     console.log(res)
+    //   }
+    // });
   };
-
-  const handleSearch =(e) =>{
-console.log(e.target.value);
-restaurants.filter((res) =>{
-  if (res.restaurantName.startsWith(e.target.value)) {
-    setRestaurants(res)
-  }
-
-})
-  }
 
   const handleLogout = () => {
     localStorage.removeItem("Token");
@@ -55,7 +73,11 @@ restaurants.filter((res) =>{
           </i>
         )}
         <i>
-          {user ? <p onClick={handleLogout}>Logout</p> : <a href="/auth">Login</a>}
+          {user ? (
+            <p onClick={handleLogout}>Logout</p>
+          ) : (
+            <a href="/auth">Login</a>
+          )}
         </i>
         <i className="search"></i>
       </ul>
@@ -81,7 +103,6 @@ restaurants.filter((res) =>{
             className="search_input"
             placeholder="search"
             type="text"
-            onClick={(e) => handleSearch(e)}
             style={{ width: isChecked ? "0" : "170px" }}
             onChange={(e) => handleSearch(e)}
           />
