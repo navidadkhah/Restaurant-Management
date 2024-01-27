f31fd9c41c519f1f9e0a52a8c273024b1d92d313
navid dadkhah <nnd1380@gmail.com>
2024-01-27 22:53:17 +0330

    add delete to restaurant

--- Frontend/src/Pages/Components/Table/Table.jsx
+++ Frontend/src/Pages/Components/Table/Table.jsx
@@ -1,6 +1,10 @@
 import "./Table.css";
+import { MdDelete } from "react-icons/md";
 
 export const Table = (props) => {
+  const handleS=(name)=>{
+    console.log(name)
+  }
   return (
     props.restaurants && (
       <table id="customers">
@@ -9,13 +13,20 @@ export const Table = (props) => {
           <th>Type</th>
           <th>Address</th>
           <th>Rate</th>
+          <th >Delete</th>
         </tr>
         {props.restaurants.map((res) => (
-          <tr>
+          <tr key={res.id}>
             <td>{res.restaurantName}</td>
             <td>{res.restaurantType}</td>
             <td>{res.restaurantLocation}</td>
             <td>{res.restaurantRate}</td>
+            <td
+              onClick={(e) => handleS(res.restaurantName)}
+              className="delete-td"
+            >
+              <MdDelete />
+            </td>
           </tr>
         ))}
       </table>
